15472f7fe8cd6a76547b38107640419a0626d5d8
Sami Finnilä <sami.finnila@gmail.com>
2024-02-16 12:37:07 +0200

    Sync improvents over from llm app
    Signed-off-by: Sami Finnilä <sami.finnila@gmail.com>

--- lib/chains.py
+++ lib/chains.py
@@ -7,40 +7,61 @@ from formalize import FormalizeChain
 from free_prompt import FreePromptChain
 from headline import HeadlineChain
 from langchain_community.llms import GPT4All
+from langchain.chains import LLMChain
 from simplify import SimplifyChain
 from summarize import SummarizeChain
 from topics import TopicsChain
+from langchain.prompts import PromptTemplate
+import json
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
+models_folder_path = os.path.join(dir_path , "../models/")
+
+def get_model_config(file_name):
+    if os.path.exists(os.path.join(models_folder_path, file_name + ".json")):
+        model_config_path = os.path.join(models_folder_path, file_name + ".json")
+        with open(model_config_path, "r") as f:
+            model_config = json.load(f)
+    else:
+        model_config_path = os.path.join(dir_path, "default_config", "config.json")
+        
+        with open(model_config_path, "r") as f:
+            default_config = json.load(f)
+        
+        if file_name in default_config:
+            model_config = default_config[file_name]
+        else:
+            model_config = default_config['default']
+    return model_config
 
 
-def generate_llm(path):
+def generate_llm_chain(file_name):
+    model_config = get_model_config(file_name)
+    
+    path = os.path.join(models_folder_path, file_name)
+    
     try:
-        return GPT4All(model=path, device="gpu", max_tokens=4096)
+        llm = GPT4All(model=path, device="gpu", **model_config['gpt4all_config'])
     except:
-        return GPT4All(model=path, device="cpu", max_tokens=4096)
+        llm = GPT4All(model=path, device="cpu", **model_config['gpt4all_config'])
+        
+    prompt = PromptTemplate.from_template(model_config['prompt'])
+        
+    return LLMChain(llm=llm, prompt=prompt)
 
-
-def generate_llm_generator(path):
-    models[file.name.split(".gguf")[0]] = lambda: generate_llm(path)
-
-
-models = {}
-
-for file in os.scandir(dir_path + "/../models/"):
-    if file.name.endswith(".gguf"):
-        generate_llm_generator(file.path)
-
-
-def generate_chains(model_name, model):
-    chains[model_name + ":summary"] = lambda: SummarizeChain(llm=model())
-    #chains[model_name + ":simplify"] = lambda: SimplifyChain(llm=model())
-    #chains[model_name + ":formalize"] = lambda: FormalizeChain(llm=model())
-    chains[model_name + ":headline"] = lambda: HeadlineChain(llm=model())
-    chains[model_name + ":topics"] = lambda: TopicsChain(llm=model())
-    chains[model_name + ":free_prompt"] = lambda: FreePromptChain(llm=model())
-
-
-chains = {}
-for model_name, model in models.items():
-    generate_chains(model_name, model)
+def generate_chains():
+    chains = {}
+    for file in os.scandir(models_folder_path):
+        if file.name.endswith(".gguf"):
+            model_name = file.name.split('.gguf')[0]
+            
+            llm_chain = generate_llm_chain(file.name)
+            
+            chains[model_name + ":summarize"] = lambda: SummarizeChain(llm_chain=llm_chain)
+            #chains[model_name + ":simplify"] = lambda: SimplifyChain(llm=llm_chain)
+            #chains[model_name + ":formalize"] = lambda: FormalizeChain(llm=llm_chain)
+            chains[model_name + ":headline"] = lambda: HeadlineChain(llm=llm_chain)
+            chains[model_name + ":topics"] = lambda: TopicsChain(llm=llm_chain)
+            chains[model_name + ":free_prompt"] = lambda: FreePromptChain(llm=llm_chain)
+            
+    return chains
--- lib/formalize.py
+++ lib/formalize.py
@@ -9,14 +9,16 @@ from langchain.chains.base import Chain
 from langchain.prompts import PromptTemplate
 from langchain.schema.prompt_template import BasePromptTemplate
 from langchain.text_splitter import CharacterTextSplitter
-from pydantic import Extra
-
+from langchain.chains import LLMChain
 
 class FormalizeChain(Chain):
-    """A formalize chain
+    """
+    A summarization chain
     """
 
-    prompt: BasePromptTemplate = PromptTemplate(
+    system_prompt = "You're an AI assistant tasked with formalizing the text given to you by the user."
+    
+    user_prompt: BasePromptTemplate = PromptTemplate(
         input_variables=["text"],
         template="""
         Rewrite the following text and rephrase it to use only formal language and be very polite:
@@ -24,17 +26,18 @@ class FormalizeChain(Chain):
         {text}
         "
         Write the above text again, rephrase it to use only formal language and be very polite.
-        """,
+        """
     )
 
+
     """Prompt object to use."""
-    llm: BaseLanguageModel
+    llm_chain: LLMChain
     output_key: str = "text"  #: :meta private:
 
     class Config:
         """Configuration for this pydantic object."""
 
-        extra = Extra.forbid
+        extra = 'forbid'
         arbitrary_types_allowed = True
 
     @property
@@ -54,18 +57,23 @@ class FormalizeChain(Chain):
         return [self.output_key]
 
     def _call(
-        self,
-        inputs: dict[str, Any],
-        run_manager: Optional[CallbackManagerForChainRun] = None,
+            self,
+            inputs: dict[str, Any],
+            run_manager: Optional[CallbackManagerForChainRun] = None,
     ) -> dict[str, str]:
+        
+        if not {"user_prompt", "system_prompt"} == set(self.llm_chain.input_keys):
+            raise ValueError("llm_chain must have input_keys ['user_prompt', 'system_prompt']")
+        if not self.llm_chain.output_keys == [self.output_key]:
+            raise ValueError(f"llm_chain must have output_keys [{self.output_key}]")
+        
         text_splitter = CharacterTextSplitter(
-            separator="\n\n|\\.|\\?|\\!", chunk_size=1000, chunk_overlap=0, keep_separator=True
-        )
-        texts = text_splitter.split_text(inputs["text"])
-        out = self.llm.generate_prompt([self.prompt.format_prompt(text=t) for t in texts])
-        texts = [t[0].text for t in out.generations]
+            separator='\n\n|\\.|\\?|\\!', chunk_size=8000, chunk_overlap=0, keep_separator=True)
+        texts = text_splitter.split_text(inputs['text'])
+        outputs = self.llm_chain.apply([{"user_prompt": self.user_prompt.format_prompt(text=t), "system_prompt": self.system_prompt} for t in texts])
+        texts = [output['text'] for output in outputs]
 
-        return {self.output_key: "\n\n".join(texts)}
+        return {self.output_key: '\n\n'.join(texts)}
 
     @property
     def _chain_type(self) -> str:
