4a16fbfef0c11acebd30df22e403651e3f671050
Marcel Klehr <mklehr@gmx.net>
2024-03-19 15:51:52 +0100

    Allow loading chains from persistent_storage

--- lib/chains.py
+++ lib/chains.py
@@ -3,16 +3,15 @@
 
 import os
 
-from formalize import FormalizeChain
 from free_prompt import FreePromptChain
 from headline import HeadlineChain
 from langchain_community.llms import GPT4All
 from langchain.chains import LLMChain
-from simplify import SimplifyChain
 from summarize import SummarizeChain
 from topics import TopicsChain
 from langchain.prompts import PromptTemplate
 import json
+from nc_py_api.ex_app import persistent_storage
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 models_folder_path = os.path.join(dir_path , "../models/")
@@ -21,6 +20,10 @@ def get_model_config(file_name):
     file_name = file_name.split('.gguf')[0]
     if os.path.exists(os.path.join(models_folder_path, file_name + ".json")):
         model_config_path = os.path.join(models_folder_path, file_name + ".json")
+        with open(model_config_path, "r") as f:
+            model_config = json.load(f)
+    elif os.path.exists(os.path.join(persistent_storage(), file_name + ".json")):
+        model_config_path = os.path.join(persistent_storage(), file_name + ".json")
         with open(model_config_path, "r") as f:
             model_config = json.load(f)
     else:
@@ -40,6 +43,9 @@ def generate_llm_chain(file_name):
 
     path = os.path.join(models_folder_path, file_name)
 
+    if not os.path.exists(path):
+        path = os.path.join(persistent_storage(), file_name)
+
     llm = GPT4All(model=path, device="cpu", **model_config['gpt4all_config'])
 
     prompt = PromptTemplate.from_template(model_config['prompt'])
@@ -55,10 +61,20 @@ def generate_chains():
             llm_chain = generate_llm_chain(file.name)
 
             chains[model_name + ":summary"] = lambda: SummarizeChain(llm_chain=llm_chain)
-            #chains[model_name + ":simplify"] = lambda: SimplifyChain(llm_chain=llm_chain)
-            #chains[model_name + ":formalize"] = lambda: FormalizeChain(llm_chain=llm_chain)
             chains[model_name + ":headline"] = lambda: HeadlineChain(llm_chain=llm_chain)
             chains[model_name + ":topics"] = lambda: TopicsChain(llm_chain=llm_chain)
             chains[model_name + ":free_prompt"] = lambda: FreePromptChain(llm_chain=llm_chain)
+
+    for file in os.scandir(persistent_storage()):
+        if file.name.endswith('.gguf'):
+            model_name = file.name.split('.gguf')[0]
+
+            llm_chain = generate_llm_chain(file.name)
+
+            chains[model_name + ":summary"] = lambda: SummarizeChain(llm_chain=llm_chain)
+            chains[model_name + ":headline"] = lambda: HeadlineChain(llm_chain=llm_chain)
+            chains[model_name + ":topics"] = lambda: TopicsChain(llm_chain=llm_chain)
+            chains[model_name + ":free_prompt"] = lambda: FreePromptChain(llm_chain=llm_chain)
+
 
     return chains
