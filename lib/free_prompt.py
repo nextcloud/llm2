babe2dc9550e59a672e63f065533403e8fc3dbad
Marcel Klehr <mklehr@gmx.net>
2025-02-11 08:12:54 +0100

    fix: add missing licenses
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>

--- lib/free_prompt.py
+++ lib/free_prompt.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
 from typing import Any, List
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_core.runnables import Runnable
--- lib/reformulate.py
+++ lib/reformulate.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
 from typing import Any
 from langchain.prompts import PromptTemplate
 from langchain.schema.prompt_template import BasePromptTemplate
