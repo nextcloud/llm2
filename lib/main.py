bb02a902f19cb5b1636c19e8bbd882a60e23b3c6
Marcel Klehr <mklehr@gmx.net>
2025-02-14 12:40:01 +0100

    enh: Improve logging
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>

--- lib/main.py
+++ lib/main.py
@@ -5,6 +5,8 @@ The main module of the llm2 app
 """
 
 import os
+import logging
+import traceback
 from contextlib import asynccontextmanager
 from json import JSONDecodeError
 from threading import Event, Thread
@@ -16,6 +18,17 @@ from fastapi import FastAPI
 from nc_py_api import AsyncNextcloudApp, NextcloudApp, NextcloudException
 from nc_py_api.ex_app import LogLvl, persistent_storage, run_app, set_handlers
 from nc_py_api.ex_app.providers.task_processing import TaskProcessingProvider, ShapeEnumValue
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', handlers=[logging.StreamHandler()])
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+def log(nc, level, content):
+    logger.log((level+1)*10, content)
+    try:
+        nc.log(level, content)
+    except:
+        pass
 
 models_to_fetch = {
     "https://huggingface.co/bartowski/Meta-Llama-3.1-8B-Instruct-GGUF/resolve/4f0c246f125fc7594238ebe7beb1435a8335f519/Meta-Llama-3.1-8B-Instruct-Q4_K_M.gguf": { "save_path": os.path.join(persistent_storage(), "Meta-Llama-3.1-8B-Instruct-Q4_K_M.gguf") },
@@ -61,7 +74,7 @@ def background_thread_task(task_processors: dict):
                 sleep(5)
                 continue
         except (NextcloudException, httpx.RequestError, JSONDecodeError) as e:
-            print("Network error fetching the next task", e, flush=True)
+            log(nc, LogLvl.ERROR, "Network error fetching the next task", e)
             sleep(5)
             continue
 
@@ -70,7 +83,7 @@ def background_thread_task(task_processors: dict):
 
         try:
             task_processor_name = provider["name"][5:]
-            print(f"chain: {task_processor_name}", flush=True)
+            log(nc, LogLvl.INFO, f"chain: {task_processor_name}")
             task_processor_loader = task_processors.get(task_processor_name)
             if task_processor_loader is None:
                 NextcloudApp().providers.task_processing.report_result(
@@ -78,26 +91,29 @@ def background_thread_task(task_processors: dict):
                 )
                 continue
             task_processor = task_processor_loader()
-            print("Generating reply", flush=True)
+            log(nc, LogLvl.INFO, "Generating reply")
             time_start = perf_counter()
-            print(task.get("input"), flush=True)
+            log(nc, LogLvl.INFO, task.get("input"))
             result = task_processor(task.get("input"))
-            print(f"reply generated: {round(float(perf_counter() - time_start), 2)}s", flush=True)
-            print(result, flush=True)
+            log(nc, LogLvl.INFO, f"reply generated: {round(float(perf_counter() - time_start), 2)}s")
+            log(nc, LogLvl.INFO, result)
             nc.providers.task_processing.report_result(
                 task["id"],
                 result,
             )
         except (NextcloudException, httpx.RequestError, JSONDecodeError) as e:
-            print("Network error:", e, flush=True)
+            # Error when reporting the result
+            exception_info = traceback.format_exception(type(e), e, e.__traceback__)
+            log(nc, LogLvl.ERROR, f"Error: {''.join(exception_info)}")
             sleep(5)
         except Exception as e:  # noqa
-            print("Error:", e, flush=True)
+            exception_info = traceback.format_exception(type(e), e, e.__traceback__)
+            log(nc, LogLvl.ERROR, f"Error: {''.join(exception_info)}")
             try:
-                nc.log(LogLvl.ERROR, str(e))
+                log(nc, LogLvl.ERROR, str(e))
                 nc.providers.task_processing.report_result(task["id"], error_message=str(e))
             except (NextcloudException, httpx.RequestError) as net_err:
-                print("Network error in reporting the error:", net_err, flush=True)
+                log(nc, LogLvl.INFO, f"Network error in reporting the error: {net_err}")
 
             sleep(5)
 
@@ -110,7 +126,7 @@ def start_bg_task():
 
 async def enabled_handler(enabled: bool, nc: AsyncNextcloudApp) -> str:
     global app_enabled
-    print(f"enabled={enabled}", flush=True)
+    log(nc, LogLvl.INFO, f"enabled={enabled}")
 
     task_processors = generate_task_processors()
 
@@ -134,31 +150,23 @@ async def enabled_handler(enabled: bool, nc: AsyncNextcloudApp) -> str:
                         } if task == "core:text2text:changetone" else {}
                 )
                 await nc.providers.task_processing.register(provider)
-                print(f"Registered {task_processor_name}", flush=True)
+                log(nc, LogLvl.INFO, f"Registered {task_processor_name}")
                 app_enabled.set()
             except Exception as e:
-                print(f"Failed to register {model} - {task}, Error: {e}\n", flush=True)
+                log(nc, LogLvl.ERROR, f"Failed to register {model} - {task}, Error: {e}\n")
                 break
     else:
         app_enabled.clear()
         for task_processor_name in task_processors:
             try:
                 await nc.providers.task_processing.unregister("llm2:" + task_processor_name)
-                print(f"Unregistered {task_processor_name}", flush=True)
+                log(nc, LogLvl.INFO, f"Unregistered {task_processor_name}")
             except Exception as e:
-                print(f"Failed to unregister {task_processor_name}, Error: {e}\n", flush=True)
+                log(nc, LogLvl.ERROR, f"Failed to unregister {task_processor_name}, Error: {e}\n")
                 break
 
     return ""
 
 
 if __name__ == "__main__":
-    # print(os.environ["APP_HOST"], flush=True)
-    # print(os.environ["APP_ID"], flush=True)
-    # print(os.environ["APP_PORT"], flush=True)
-    # print(os.environ["APP_SECRET"], flush=True)
-    # print(os.environ["APP_VERSION"], flush=True)
-    # print(os.environ["COMPUTE_DEVICE"], flush=True)
-    # print(os.environ["NEXTCLOUD_URL"], flush=True)
-    # print(os.environ["APP_PERSISTENT_STORAGE"], flush=True)
     run_app("main:APP", log_level="trace")
