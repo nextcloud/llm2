466b752899af13791137be5e5374aca4b131c6d1
Marcel Klehr <mklehr@gmx.net>
2025-03-21 10:22:23 +0100

    fix(contextwrite): refactor prompt to work on languages other than english
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>

--- lib/contextwrite.py
+++ lib/contextwrite.py
@@ -22,17 +22,16 @@ class ContextWriteProcessor:
         input_variables=["style_input", "source_input"],
         template="""
 You're a professional copywriter tasked with copying an instructed or demonstrated *WRITING STYLE* and writing a text on the provided *SOURCE MATERIAL*.
-*WRITING STYLE*:
-{style_input}
-
-*SOURCE MATERIAL*:
-{source_input}
-
 Now write a text in the same style detailed or demonstrated under *WRITING STYLE* using the *SOURCE MATERIAL* as source of facts and instruction on what to write about.
 Do not invent any facts or events yourself. Also, use the *WRITING STYLE* as a guide for how to write the text ONLY and not as a source of facts or events.
 Detect the language used in the *SOURCE_MATERIAL*. Make sure to use the detected language in your text. Do not mention the language you detected explicitly.
 Only output the newly written text without quotes, nothing else, no introductory or explanatory text.
-        """
+
+*WRITING STYLE*:
+{style_input}
+
+*SOURCE MATERIAL*:
+{source_input}"""
     )
     def __init__(self, runnable: Runnable):
         self.runnable = runnable
