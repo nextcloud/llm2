b84fc0653a3bed027c6418a3c89374c4705548e3
Marcel Klehr <mklehr@gmx.net>
2025-04-18 12:16:29 +0200

    feat(chatwithtools): Make tool calling work with "normal" chat models
    by simulating tool calls in the chat, invisible to the user
    
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>

--- lib/chatwithtools.py
+++ lib/chatwithtools.py
@@ -3,6 +3,7 @@
 """A chat chain
 """
 import json
+import pprint
 import re
 from random import randint
 from typing import Any
@@ -15,11 +16,17 @@ def try_parse_tool_calls(content: str):
     """Try parse the tool calls."""
     tool_calls = []
     offset = 0
-    for i, m in enumerate(re.finditer(r"<tool_call>\n(.+)?\n</?tool_call>", content)):
+    found = False
+    # Qwen-style tool call, works with Llama 3.1
+    # <tool_call>{"name": "function_name", "arguments": {"param1": "value1", "param2": "value2"}}</tool_call>
+    for i, m in enumerate(re.finditer(r"<tool_call>\w*?(.+)?\w*?</?tool_call>", content)):
         if i == 0:
             offset = m.start()
         try:
-            func = json.loads(m.group(1))
+            try:
+                func = json.loads(m.group(1))
+            except json.JSONDecodeError as e:
+                func = json.loads(m.group(1)[0:-1])
             tool_calls.append(func)
             if isinstance(func["arguments"], str):
                 func["arguments"] = json.loads(func["arguments"])
@@ -28,9 +35,36 @@ def try_parse_tool_calls(content: str):
                 del func['arguments']
             if not 'id' in func:
                 func['id'] = str(randint(1, 10000000000))
+            found = True
         except json.JSONDecodeError as e:
             print(f"Failed to parse tool calls: the content is {m.group(1)} and {e}")
             pass
+
+    if not found:
+        # Gemma-style tool call
+        # ```tool_call
+        # {"name": "function_name", "arguments": {"param1": "value1", "param2": "value2"}}
+        # ```
+        for i, m in enumerate(re.finditer(r"```tool_call\n(.+)?\n```", content)):
+            if i == 0:
+                offset = m.start()
+            try:
+                try:
+                    func = json.loads(m.group(1))
+                except json.JSONDecodeError as e:
+                    func = json.loads(m.group(1)[0:-1])
+                tool_calls.append(func)
+                if isinstance(func["arguments"], str):
+                    func["arguments"] = json.loads(func["arguments"])
+                if 'arguments' in func:
+                    func['args'] = func['arguments']
+                    del func['arguments']
+                if not 'id' in func:
+                    func['id'] = str(randint(1, 10000000000))
+            except json.JSONDecodeError as e:
+                print(f"Failed to parse tool calls: the content is {m.group(1)} and {e}")
+                pass
+
     if tool_calls:
         if offset > 0 and content[:offset].strip():
             c = content[:offset]
@@ -50,35 +84,65 @@ class ChatWithToolsProcessor:
         self.model = runner
 
     def _process_single_input(self, input_data: dict[str, Any]) -> dict[str, Any]:
-        model_with_tools = self.model.bind_tools(json.loads(input_data['tools']))
+        system_prompt = """
+{downstream_system_prompt}
+
+You have tools at your disposal that you can call on behalf of the user.
+You can call a tool by responding with a tool call.
+A tool call starts with an opening `tool_call` xml tag, then a JSON object with the name of the function and the arguments, and finally it ends with a closing `tool_call` xml tag.
+It looks like this, for example:
+{tool_call_example}
+
+Here is a second example:
+{tool_call_example2}
+
+When calling tools, do not output anything else, except the tool call. Do not add sample output of the tool call. Do not output the result of the tool call yourself.
+The following is a JSON specification of the tools you can call and their parameters.
+{tools}
+""".format(
+            tools=input_data['tools'],
+            downstream_system_prompt=input_data['system_prompt'],
+            tool_call_example='<tool_call>{"name": "the_function_to_call", "arguments": {"param1": "the first argument", "param2": "second argument"}}</tool_call>',
+            tool_call_example2='<tool_call>{"name": "search_the_web", "arguments": {"search_query": "Frank Sinatra"}}</tool_call>'
+        )
 
         messages = []
-        messages.append(SystemMessage(content=input_data['system_prompt']))
+        messages.append(SystemMessage(content=system_prompt))
 
         for raw_message in input_data['history']:
             message = json.loads(raw_message)
             if message['role'] == 'assistant':
-                messages.append(AIMessage(content=input_data['system_prompt']))
+                if message['content'] == '':
+                    continue
+                messages.append(AIMessage(content=message['content']))
             elif message['role'] == 'human':
-                messages.append(HumanMessage(content=input_data['system_prompt']))
+                messages.append(HumanMessage(content=message['content']))
 
-        messages.append(HumanMessage(content=input_data['input']))
+        if input_data['input'] != '':
+            messages.append(HumanMessage(content=input_data['input']))
+        elif 'tool_message' in input_data and input_data['tool_message'] != '':
+            try:
+                tool_messages = json.loads(input_data['tool_message'])
+                for tool_message in tool_messages:
+                    message_content = """
+    The result of your tool call for the tool "{tool_name}" is the following:
+    {tool_call_result}
+    You can now formulate this in natural language for the user. Do not mention that you called a tool.
+    """.format(tool_call_result=tool_message['content'], tool_name=tool_message['name'])
+                    messages.append(HumanMessage(
+                        content=message_content
+                    ))
+            except json.JSONDecodeError as e:
+                print('Failed to parse tool message')
+                print(e)
+        else:
+            messages.append(HumanMessage(content=''))
 
-        try:
-            tool_messages = json.loads(input_data['tool_message'])
-            for tool_message in tool_messages:
-                messages.append(ToolMessage(
-                    content=tool_message['content'],
-                    name=tool_message['name'],
-                    tool_call_id='42'
-                ))
-        except:
-            pass
+        pprint.pprint(messages)
+        response = self.model.invoke(messages)
 
-        response = model_with_tools.invoke(messages)
-
-        if not response.tool_calls or len(response.tool_calls) == 0:
-            response = AIMessage(**try_parse_tool_calls(response.content))
+        #if not response.tool_calls or len(response.tool_calls) == 0:
+        response = AIMessage(**try_parse_tool_calls(response.content))
 
         return {
             'output': response.content,
