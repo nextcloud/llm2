a5f6b821fad811017b96532786ee84fe9ed5aeee
Marcel Klehr <mklehr@gmx.net>
2025-05-06 15:45:01 +0200

    fix(chatwithtools): Allow llm to see its own tool call in the chat history
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>

--- lib/chatwithtools.py
+++ lib/chatwithtools.py
@@ -12,6 +12,12 @@ from langchain_community.chat_models import ChatLlamaCpp
 from langchain_core.messages import SystemMessage, HumanMessage, ToolMessage
 from langchain_core.messages.ai import AIMessage
 
+def generate_tool_call(tool_call: dict):
+    content = '<tool_call>'
+    content += json.dumps({"name": tool_call['name'], "arguments": tool_call['args']})
+    content += '</tool_call>'
+    return content
+
 def try_parse_tool_calls(content: str):
     """Try parse the tool calls."""
     tool_calls = []
@@ -113,8 +119,12 @@ The following is a JSON specification of the tools you can call and their parame
             message = json.loads(raw_message)
             if message['role'] == 'assistant':
                 if message['content'] == '':
-                    continue
-                messages.append(AIMessage(content=message['content']))
+                    if 'tool_calls' not in message or len(message['tool_calls']) == 0:
+                        continue
+                    else:
+                        messages.append(AIMessage(content=generate_tool_call(message['tool_calls'][0])))
+                else:
+                    messages.append(AIMessage(content=message['content']))
             elif message['role'] == 'human':
                 messages.append(HumanMessage(content=message['content']))
 
